c524d95458bfd8413bd028c27d7fedbcef3a330e
Sait Ramazan <70922350+srgul@users.noreply.github.com>
2023-04-12 00:22:04 +0300

    every todo method updated

--- src/todo/todo/todo.controller.ts
+++ src/todo/todo/todo.controller.ts
@@ -11,46 +11,32 @@ import { TodoDTO } from './todo.dto';
 import { todos } from './todo-mock';
 import { TodoService } from './todo.service';
 
-let todosData = todos;
-
 @Controller('todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Get()
   getTodos(): TodoDTO[] {
-    return this.todoService.GetAll();
+    return this.todoService.getAll();
   }
 
   @Get(':id')
   getTodo(@Param('id') id: string): TodoDTO {
-    return this.todoService.findOne(id);
+    return this.todoService.get(id);
   }
 
   @Post()
-  createTodo(@Body() createTodo: TodoDTO): TodoDTO {
-    const newTodo: TodoDTO = {
-      id: (todosData.length + 1).toString(),
-      ...createTodo,
-    };
-
-    todosData = [...todosData, newTodo];
-
-    return newTodo;
+  createTodo(@Body() createTodo: TodoDTO) {
+    return this.todoService.create(createTodo);
   }
 
   @Put(':id')
   updateTodo(@Body() updateTodo: TodoDTO, @Param('id') id): TodoDTO {
-    todosData = todosData.map((todo) => (todo.id === id ? updateTodo : todo));
-
-    return updateTodo;
+    return this.todoService.updateTodo(updateTodo, id);
   }
 
   @Delete(':id')
   deleteTodo(@Param('id') id): TodoDTO {
-    const todoToDelete = todosData.find((todo) => todo.id === id);
-    todosData = todosData.filter((todo) => todo.id !== id);
-
-    return todoToDelete;
+    return this.todoService.delete(id);
   }
 }
--- src/todo/todo/todo.service.ts
+++ src/todo/todo/todo.service.ts
@@ -4,14 +4,32 @@ import { TodoDTO } from './todo.dto';
 
 @Injectable()
 export class TodoService {
-  private readonly todosData: TodoDTO[] = todos;
+  private todosData: TodoDTO[] = todos;
 
-  GetAll(): TodoDTO[] {
+  getAll(): TodoDTO[] {
     return this.todosData;
   }
 
-  findOne(id: string): TodoDTO {
-    console.log(id);
+  get(id: string): TodoDTO {
     return this.todosData.find((x: TodoDTO) => x.id === id);
+  }
+
+  create(newTodo: any): TodoDTO {
+    const todo: TodoDTO = { id: this.todosData.length + 1, ...newTodo };
+    this.todosData.push(todo);
+    return todo;
+  }
+
+  updateTodo(updateTodo: TodoDTO, id: string): TodoDTO {
+    const data = this.todosData.find((x: TodoDTO) => x.id === id);
+    data.title = updateTodo.title;
+    data.status = updateTodo.status;
+    return data;
+  }
+
+  delete(id: string): any {
+    const deletedTodo = this.todosData.find((todo: TodoDTO) => todo.id === id);
+    this.todosData = this.todosData.filter((todo: TodoDTO) => todo.id !== id);
+    return deletedTodo;
   }
 }
