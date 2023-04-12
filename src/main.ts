092164c9d9fa4d31127afa88d0390ffc952bac89
Sait Ramazan <70922350+srgul@users.noreply.github.com>
2023-04-12 23:33:39 +0300

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Base api example')
+        .setDescription('The base API description')
+        .setVersion('1.0')
+        .addTag('base')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3000);
 }
 bootstrap();
