72b7bdaf82744a3f7537ed54b76333292aa30bde
Sait Ramazan <70922350+srgul@users.noreply.github.com>
2023-04-14 05:15:52 +0300

    some changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,20 @@ import { TodoService } from './todo/todo/todo.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { EmailController } from './helper/email/email.controller';
+import { EmailService } from './helper/email/email.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
         MongooseModule.forRoot('mongodb://localhost/nest'),
         UsersModule,
         AuthModule,
+        ConfigModule.forRoot({
+            envFilePath: ['.env'],
+        }),
     ],
-    controllers: [AppController, TodoController],
-    providers: [AppService, TodoService],
+    controllers: [AppController, TodoController, EmailController],
+    providers: [AppService, TodoService, EmailService],
 })
 export class AppModule {}
